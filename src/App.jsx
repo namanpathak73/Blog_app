6aa69d31d0e640277a405ac32cbe8b5e17e59798
Naman Pathak <namanpathak73@gmail.com>
2024-02-23 21:55:13 +0530

    Blog APP

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import Header  from "./components/header/Header.jsx";
+import Footer from "./components/footer/Footer.jsx"
+import { Outlet } from "react-router-dom";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({ userData }));
+      } else {
+        dispatch(logout());
+      }
+    })
+    .finally(() => setLoading(false))
+  }, []);
+
+  return !loading ? (
+    <div className=" min-h-screen  flex flex-wrap content-between bg-gray-600"> 
+    <div className=" w-full block">
+      <Header />
+      Todo: <Outlet />
+      <Footer />
+    </div>
+    </div>
+  ) : null
+}
+
+export default App;
