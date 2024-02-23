363827d2d6a99d7691b0b41c2ba20e640e358fd5
Naman Pathak <namanpathak73@gmail.com>
2024-02-23 23:35:57 +0530

    Create react components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,13 @@
 import Header from "./header/header";
 import Footer from "./footer/footer";
+import Container from "postcss/lib/container";
+import Logo from "./Logo";
+import LogoutBtn from "./header/LogoutBtn";
 
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
 }
